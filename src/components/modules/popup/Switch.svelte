71fdb6ac593841530d061e2cba5bb360fed1b0c3
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-02-29 20:07:02 +0330

    feat: simplify switch component with mui

--- src/components/modules/popup/Switch.svelte
+++ src/components/modules/popup/Switch.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
+  import type { MdSwitch } from "@material/web/switch/switch";
 
-  export let design = "inner label";
-  export let options: string[] = [];
-  export let fontSize = 16;
   export let value: string;
 
   let checked = true;
-
-  const uniqueID = Math.floor(Math.random() * 100);
   const dispatch = createEventDispatcher();
 
-  function handleClick(event: Event) {
-    const target = event.target as HTMLElement;
-    const state = target.getAttribute("aria-checked");
-    checked = state === "true" ? false : true;
+  function handleChange(event: InputEvent) {
+    checked = (event.target as MdSwitch).selected;
     value = checked ? "on" : "off";
     dispatch("change", value);
   }
@@ -22,212 +16,17 @@
   $: value === "on" ? (checked = true) : (checked = false);
 </script>
 
-{#if design == "inner"}
-  <div class="s s--inner">
-    <button
-      style="cursor: pointer;"
-      role="switch"
-      aria-checked={checked}
-      aria-labelledby={`switch-${uniqueID}`}
-      on:click={handleClick}>
-      <span>on</span>
-      <span>off</span>
-    </button>
-  </div>
-{:else if design == "slider"}
-  <div class="s s--slider" style="font-size:{fontSize}px">
-    <button role="switch" aria-checked={checked} aria-labelledby={`switch-${uniqueID}`} on:click={handleClick}>
-    </button>
-  </div>
-{:else}
-  <div class="s s--multi">
-    <div
-      role="radiogroup"
-      class="group-container"
-      aria-labelledby={`label-${uniqueID}`}
-      style="font-size:{fontSize}px"
-      id={`group-${uniqueID}`}>
-      {#each options as option}
-        <input type="radio" id={`${option}-${uniqueID}`} value={option} bind:group={value} />
-        <label for={`${option}-${uniqueID}`}>
-          {option}
-        </label>
-      {/each}
-    </div>
-  </div>
-{/if}
+<div class="switch-container">
+  <span>OFF</span>
+  <md-switch role="switch" aria-checked={checked} on:change={handleChange} />
+  <span>ON</span>
+</div>
 
-<style>
-  :root {
-    --accent-color: #0f7dff;
-    --gray: #ccc;
-  }
-  .option {
-    cursor: pointer;
-  }
-  /* Inner Design Option */
-  .s--inner button {
-    padding: 0.5em;
-    background-color: #fff;
-    border-radius: 0.2rem;
-    border: 1px solid var(--gray);
-  }
-  [role="switch"][aria-checked="true"] :first-child,
-  [role="switch"][aria-checked="false"] :last-child {
-    display: none;
-    color: #fff;
-  }
-
-  .s--inner button span {
-    user-select: none;
-    pointer-events: none;
-    padding: 0.25em 0.8rem;
-  }
-
-  /* Slider Design Option */
-
-  .s--slider {
+<style scoped>
+  .switch-container {
     display: flex;
     justify-content: center;
+    gap: 0.8rem;
     align-items: center;
-  }
-
-  .s--slider button {
-    width: 3em;
-    height: 1.6em;
-    position: relative;
-    margin: 0 0 0 0.5em;
-    background: var(--gray);
-    border: none;
-  }
-
-  .s--slider button::before {
-    content: "";
-    position: absolute;
-    width: 1.3em;
-    height: 1.3em;
-    background: #fff;
-    top: 0.13em;
-    right: 1.5em;
-    transition: transform 0.3s;
-  }
-
-  .s--slider button[aria-checked="true"] {
-    background-color: var(--accent-color);
-  }
-
-  .s--slider button[aria-checked="true"]::before {
-    transform: translateX(1.3em);
-    transition: transform 0.3s;
-  }
-
-  .s--slider button:focus {
-    box-shadow: 0 0px 0px 1px var(--accent-color);
-  }
-
-  /* Multi Design Option */
-
-  /* Based on suggestions from Sara Soueidan https://www.sarasoueidan.com/blog/toggle-switch-design/
-    and this example from Scott O'hara https://codepen.io/scottohara/pen/zLZwNv */
-
-  .s--multi .group-container {
-    border: none;
-    padding: 0;
-    white-space: nowrap;
-  }
-
-  /* .s--multi legend {
-    font-size: 2px;
-    opacity: 0;
-    position: absolute;
-    } */
-
-  .s--multi label {
-    display: inline-block;
-    line-height: 1.6;
-    position: relative;
-    z-index: 2;
-  }
-
-  .s--multi input {
-    opacity: 0;
-    position: absolute;
-  }
-
-  .s--multi label:first-of-type {
-    padding-right: 5em;
-  }
-
-  .s--multi label:last-child {
-    margin-left: -5em;
-    padding-left: 5em;
-  }
-
-  .s--multi:focus-within label:first-of-type:after {
-    box-shadow: 0 0px 8px var(--accent-color);
-    border-radius: 1.5em;
-  }
-
-  /* making the switch UI.  */
-  .s--multi label:first-of-type:before,
-  .s--multi label:first-of-type:after {
-    content: "";
-    height: 1.25em;
-    overflow: hidden;
-    pointer-events: none;
-    position: absolute;
-    vertical-align: middle;
-  }
-
-  .s--multi label:first-of-type:before {
-    border-radius: 100%;
-    z-index: 2;
-    position: absolute;
-    width: 1.2em;
-    height: 1.2em;
-    background: #fff;
-    top: 0.2em;
-    right: 1.2em;
-    transition: transform 0.3s;
-  }
-
-  .s--multi label:first-of-type:after {
-    background: var(--accent-color);
-    border-radius: 1em;
-    margin: 0 1em;
-    transition: background 0.2s ease-in-out;
-    width: 3em;
-    height: 1.6em;
-  }
-
-  .s--multi input:first-of-type:checked ~ label:first-of-type:after {
-    background: var(--gray);
-  }
-
-  .s--multi input:first-of-type:checked ~ label:first-of-type:before {
-    transform: translateX(-1.4em);
-  }
-
-  .s--multi input:last-of-type:checked ~ label:last-of-type {
-    z-index: 1;
-  }
-
-  /* gravy */
-
-  /* Inner Design Option */
-  [role="switch"][aria-checked="true"] :first-child,
-  [role="switch"][aria-checked="false"] :last-child {
-    border-radius: 0.25em;
-    background: var(--accent-color);
-    display: inline-block;
-  }
-
-  /* Slider Design Option */
-  .s--slider button {
-    border-radius: 1.5em;
-  }
-
-  .s--slider button::before {
-    border-radius: 100%;
   }
 </style>
